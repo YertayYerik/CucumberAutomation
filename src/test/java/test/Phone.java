c0d820470325b39d137ce8358da295625844efc0
YertayYerik <yertayyerik@gmail.com>
2023-03-15 12:38:28 -0500

    initial commit

---
+++ src/test/java/test/Phone.java
@@ -0,0 +1,14 @@
+package test;
+
+import com.google.common.annotations.VisibleForTesting;
+import utilities.Driver;
+
+public class Phone {
+
+    public static void main(String[] args) {
+
+        Driver.getDriver().get("https://google.com");
+    }
+
+
+}
